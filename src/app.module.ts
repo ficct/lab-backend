ee1076ad30d3423a3ed8608e28cd1b8d36b3a081
cartory <xXcartoryXx@gmail.com>
2021-12-16 22:22:33 -0400

    update app > controller, module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
+import { TaskModule } from './modules/tasks/task.module';
+import { ScheduleModule } from './modules/schedules/schedule.module';
 import { InventoryModule } from './modules/inventory/inventory.module';
-import { TaskModule } from './modules/task/task.module';
-import { ScheduleModule } from './modules/schedule/schedule.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
-    InventoryModule,
     TaskModule,
     ScheduleModule,
+    InventoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
