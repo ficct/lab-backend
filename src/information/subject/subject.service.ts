28a5fbfa35fbcdc9e0adefc549f7730c3882e397
cartory <xXcartoryXx@gmail.com>
2022-01-11 15:27:31 -0400

    update subject.service

--- src/information/subject/subject.service.ts
+++ src/information/subject/subject.service.ts
@@ -4,9 +4,41 @@ import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 
 import { Subject } from 'entities/Subject';
 
+interface QuerySubjectOptions {
+  page: number;
+  limit: number;
+  careerId?: number;
+  semester?: number;
+}
+
 @Injectable()
 export class SubjectService extends TypeOrmCrudService<Subject> {
   constructor(@InjectRepository(Subject) repo) {
     super(repo);
+  }
+
+  async getSubjectQuery(query: QuerySubjectOptions): Promise<Subject[]> {
+    const { limit, page, careerId, semester } = query;
+    console.log(query);
+
+    try {
+      if (careerId && semester) {
+        return await this.repo
+          .createQueryBuilder('s')
+          .distinct(true)
+          .innerJoin('s.Career', 'c')
+          .innerJoin('s.Subject_Career', 'sc')
+          .where('c.id = :careerID', { careerId })
+          .where('sc.semester = :semester', { semester })
+          .take(limit)
+          .skip(limit * page)
+          .getMany();
+      }
+
+      return await this.repo.find({ take: limit, skip: limit * page });
+    } catch (err) {
+      console.error(err);
+    }
+    return [];
   }
 }
