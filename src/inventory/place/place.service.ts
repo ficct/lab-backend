1d2c1f09806e27f41a0aac10d60f2a9f3b9d2fc6
cartory <xXcartoryXx@gmail.com>
2022-01-12 14:43:59 -0400

    update place.service > getOne

--- src/inventory/place/place.service.ts
+++ src/inventory/place/place.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { CrudRequest, GetManyDefaultResponse } from '@nestjsx/crud';
 
 import { Place } from 'entities/Place';
+import { PlaceTypeService } from './place-type/place-type.service';
 
 @Injectable()
 export class PlaceService extends TypeOrmCrudService<Place> {
-  constructor(@InjectRepository(Place) repo) {
+  constructor(@InjectRepository(Place) repo, private type: PlaceTypeService) {
     super(repo);
+  }
+
+  async getMany(
+    req: CrudRequest,
+  ): Promise<Place[] | GetManyDefaultResponse<Place>> {
+    const { page = 0, limit = 10 } = req.parsed;
+    const { id } = await this.type.findOne({ order: { id: 'ASC' } });
+
+    return this.repo.find({
+      take: limit,
+      skip: page * limit,
+      where: { typeid: id },
+      relations: ['type', 'places', 'places.type'],
+    });
+  }
+
+  getOne(req: CrudRequest): Promise<Place> {
+    const [paramId] = req.parsed.paramsFilter;
+
+    return this.repo.findOne(paramId.value, {
+      relations: ['type', 'places', 'places.type'],
+    });
   }
 }
