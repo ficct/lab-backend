807a50286e3205c4e8a7e7fc2342e3e741dbab7b
cartory <xXcartoryXx@gmail.com>
2022-01-12 14:45:06 -0400

    update information.module

--- src/information/information.module.ts
+++ src/information/information.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+
 import { JobModule } from './job/job.module';
 import { CareerModule } from './career/career.module';
 import { SubjectModule } from './subject/subject.module';
 
 @Module({
-  imports: [JobModule, CareerModule, SubjectModule]
+  imports: [JobModule, CareerModule, SubjectModule],
 })
 export class InformationModule {}
