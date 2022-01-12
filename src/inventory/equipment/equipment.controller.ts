94966c405d2bffc133404b0cd31ac52c7516c663
cartory <xXcartoryXx@gmail.com>
2022-01-12 14:43:43 -0400

    update equipment.controller

--- src/inventory/equipment/equipment.controller.ts
+++ src/inventory/equipment/equipment.controller.ts
@@ -1,5 +1,7 @@
-import { Controller } from '@nestjs/common';
+import { Response } from 'express';
+
 import { Crud, CrudController } from '@nestjsx/crud';
+import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 
 import { Equipment } from 'entities/Equipment';
 import { EquipmentService } from './equipment.service';
@@ -10,4 +12,26 @@ import { EquipmentService } from './equipment.service';
 @Controller('equipments')
 export class EquipmentController implements CrudController<Equipment> {
   constructor(public service: EquipmentService) {}
+
+  @Get('/')
+  async findAll(@Query() query, @Res() res: Response<Equipment[]>) {
+    let { placeId } = query;
+    const { limit = 10, page = 0, search = null } = query;
+
+    placeId = Number.parseInt(placeId);
+
+    try {
+      return res.status(HttpStatus.ACCEPTED).send(
+        await this.service.getManyWithQuery({
+          page,
+          limit,
+          search,
+          placeId: isNaN(placeId) ? null : placeId,
+        }),
+      );
+    } catch (err) {
+      console.error(err);
+    }
+    return res.status(HttpStatus.NO_CONTENT).send([]);
+  }
 }
