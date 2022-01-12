71dd62c2ff1cb9b8c909479ac22f20dbd55dd155
cartory <xXcartoryXx@gmail.com>
2022-01-12 00:48:31 -0400

    update subjectCareer > with Career

--- src/entities/SubjectCareer.ts
+++ src/entities/SubjectCareer.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm';
 import { Subject } from './Subject';
-import { Career } from './Career';
+// import { Career } from './Career';
 
 @Index('FKSubject_Ca801092', ['careerid'], {})
 @Entity('Subject_Career', { schema: 'ficct' })
@@ -18,13 +18,13 @@ export class SubjectCareer {
     onDelete: 'NO ACTION',
     onUpdate: 'NO ACTION',
   })
-  @JoinColumn([{ name: 'Subjectid', referencedColumnName: 'id' }])
+  @JoinColumn({ name: 'Subjectid', referencedColumnName: 'id' })
   subject: Subject;
 
-  @ManyToOne(() => Career, (career) => career.subjectCareers, {
-    onDelete: 'NO ACTION',
-    onUpdate: 'NO ACTION',
-  })
-  @JoinColumn([{ name: 'Careerid', referencedColumnName: 'id' }])
-  career: Career;
+  // @ManyToOne(() => Career, (career) => career.subjectCareers, {
+  //   onDelete: 'NO ACTION',
+  //   onUpdate: 'NO ACTION',
+  // })
+  // @JoinColumn([{ name: 'Careerid', referencedColumnName: 'id' }])
+  // career: Career;
 }
