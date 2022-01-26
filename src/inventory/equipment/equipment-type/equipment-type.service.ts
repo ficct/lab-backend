d488aad7d40b90c1f4b5f61de9bb2e6ac9a6f95a
cartory <xXcartoryXx@gmail.com>
2022-01-25 22:39:57 -0400

    add equipmentType > seeder

--- src/inventory/equipment/equipment-type/equipment-type.service.ts
+++ src/inventory/equipment/equipment-type/equipment-type.service.ts
@@ -2,11 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 
+import equipment_types from './types.json';
 import { EquipmentType } from 'entities/EquipmentType';
 
 @Injectable()
 export class EquipmentTypeService extends TypeOrmCrudService<EquipmentType> {
   constructor(@InjectRepository(EquipmentType) repo) {
     super(repo);
+  }
+
+  async seed() {
+    const count = await this.repo.count();
+    if (!count) {
+      await this.repo
+        .createQueryBuilder()
+        .insert()
+        .into(EquipmentType)
+        .values(equipment_types)
+        .execute();
+    }
   }
 }
