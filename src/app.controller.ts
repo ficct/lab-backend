dfdd684cd3e24b5d9972c10ef0686e4ad7fcac80
cartory <xXcartoryXx@gmail.com>
2022-01-26 18:32:48 -0400

    update app > add > interval-backup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,13 @@
 import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
 
-import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
 
 @Controller()
 @ApiExcludeController()
 export class AppController {
-  constructor(
-    private authService: AuthService,
-    private readonly appService: AppService,
-  ) {
-    this.appService.seedDB().finally(() => null);
-  }
+  constructor(private authService: AuthService) {}
 
   @Get()
   getDocs(@Res() res): string {
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,29 +3,8 @@ import mysqldump from 'mysqldump';
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
-import { JobService } from './information/job/job.service';
-import { CareerService } from './information/career/career.service';
-import { SubjectService } from './information/subject/subject.service';
-
-import { EquipmentTypeService } from './inventory/equipment/equipment-type/equipment-type.service';
-import { EquipmentUnitService } from './inventory/equipment/equipment-unit/equipment-unit.service';
-import { EquipmentBrandService } from './inventory/equipment/equipment-brand/equipment-brand.service';
-
 @Injectable()
 export class AppService {
-  private readonly logger = new Logger('BACKUP');
-
-  constructor(
-    // information
-    private jobService: JobService,
-    private careerService: CareerService,
-    private subjectService: SubjectService,
-    // inventory
-    private unitService: EquipmentUnitService,
-    private typeService: EquipmentTypeService,
-    private brandService: EquipmentBrandService,
-  ) {}
-
   get DB_connection() {
     return {
       user: process.env.DB_USER,
@@ -36,17 +15,18 @@ export class AppService {
     };
   }
 
+  private readonly logger = new Logger('BACKUP');
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
-  backupDB(): void {
+  backupDB() {
     Promise.all([
       mysqldump({
         connection: this.DB_connection,
-        dumpToFile: 'export/struct.sql',
+        dumpToFile: 'backup/struct.sql',
         dump: { data: false },
       }),
       mysqldump({
         connection: this.DB_connection,
-        dumpToFile: 'export/data.sql',
+        dumpToFile: 'backup/data.sql',
         dump: {
           schema: false,
           trigger: false,
@@ -56,22 +36,5 @@ export class AppService {
     ])
       .then(() => this.logger.log(`BACKUP DONE, ${new Date()}`))
       .catch((e) => this.logger.error(e));
-  }
-
-  async seedDB() {
-    try {
-      // information
-      await this.jobService.seed();
-      const careers = await this.careerService.seed();
-      await this.subjectService.seed(careers);
-      // inventory
-      await Promise.all([
-        this.typeService.seed(),
-        this.unitService.seed(),
-        this.brandService.seed(),
-      ]);
-    } catch (err) {
-      console.error(err);
-    }
   }
 }
