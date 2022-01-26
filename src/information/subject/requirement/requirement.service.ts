abd49664071e013e2def140babcbebd4b28fd203
cartory <xXcartoryXx@gmail.com>
2022-01-26 15:02:40 -0400

    update subject > seeder

--- src/information/subject/requirement/requirement.service.ts
+++ src/information/subject/requirement/requirement.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 
+import { Repository } from 'typeorm';
 import { Requirement } from 'entities/Requirement';
 
 @Injectable()
 export class RequirementService extends TypeOrmCrudService<Requirement> {
-  constructor(@InjectRepository(Requirement) repo) {
+  constructor(
+    @InjectRepository(Requirement) public repo: Repository<Requirement>,
+  ) {
     super(repo);
   }
 }
